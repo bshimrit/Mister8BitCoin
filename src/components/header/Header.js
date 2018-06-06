34255763d37202a60bcfb649cc430dd74aea0279
bshimrit <bshimrit@gmail.com>
2018-06-06 09:30:24 +0300

    images

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -5,24 +5,24 @@ import './header.css'
 const Header = () => {
     return (
         <header className="container App-header">
-            <img className="logo" src="./img/logo.png" />
+            <img className="logo" src="img/logo.png" />
             <h1>Mister 8BitCoin</h1>
             <div className="nav-bar">
                 <NavLink exact to="/">
                     <div>
-                        <img src="./img/icons/home.jpg" />
+                        <img src="img/icons/home.jpg" />
                         <div> HOME </div>
                     </div>
                 </NavLink>
                 <NavLink exact to="/contacts">
                     <div>
-                        <img src="./img/icons/users.jpg" />
+                        <img src="img/icons/users.jpg" />
                         <div> CONTACTS </div>
                     </div>
                 </NavLink>
                 <NavLink exact to="/moves">
                     <div>
-                        <img src="./img/icons/ledger.jpg" />
+                        <img src="img/icons/ledger.jpg" />
                         <div> LEDGER </div>
                     </div>
                 </NavLink>
