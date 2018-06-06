9717321782cff02d3c631b164dec0e62145642c1
bshimrit <bshimrit@gmail.com>
2018-06-06 09:22:25 +0300

    image

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -5,7 +5,7 @@ import './header.css'
 const Header = () => {
     return (
         <header className="container App-header">
-            <img className="logo" src="img/logo.png" />
+            <img className="logo" src="./img/logo.png" />
             <h1>Mister 8BitCoin</h1>
             <div className="nav-bar">
                 <NavLink exact to="/">
